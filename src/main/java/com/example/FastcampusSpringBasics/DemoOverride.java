9f3b19f1b6a0ea03e354ff52ab1dfe75c416aec4
JuHyeonPark <parkjh080@naver.com>
2023-10-10 19:22:43 +0900

    feat(study): @Override 어노테이션 다루기 2023-10-10
    @Override 어노테이션의 역할과 상속과 재정의에 대해 공부

---
+++ src/main/java/com/example/FastcampusSpringBasics/DemoOverride.java
@@ -0,0 +1,22 @@
+package com.example.FastcampusSpringBasics;
+
+public class DemoOverride {
+
+}
+
+class Parent {
+    void test(String t) {
+
+    }
+}
+
+class Child extends Parent{
+
+    void test(String t) {
+
+    }
+
+    void test(String t, String s) {
+
+    }
+}
