ae299e13824cf707b5acfaeb56a4028709bcf656
JuHyeonPark <parkjh080@naver.com>
2023-10-10 19:19:59 +0900

    feat(study): AppConfig.java 생성 2023-10-10
    IoC에서 스프링 컨테이너 역할을 대신 해줄 AppConfig.java 생성 @Configuration 과 @ Bean
    어노테이션을 사용하여 스프링 빈 지정하기

---
+++ src/main/java/com/example/FastcampusSpringBasics/AppConfig.java
@@ -0,0 +1,16 @@
+package com.example.FastcampusSpringBasics;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// 이 클래스 안에 객체 생성하는 메소드 보이지?
+// 그 메소드가 반환하는 객체를 "스프링 빈"으로 등록 해줘 = 스프링 컨테이너에 등록 해줘
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService();
+    }
+}
