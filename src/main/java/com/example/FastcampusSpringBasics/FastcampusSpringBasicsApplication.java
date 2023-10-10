9ad5ab43ffe11239ec08cd334af49920ca7de082
JuHyeonPark <parkjh080@naver.com>
2023-10-10 19:16:45 +0900

    feat: 초기 셋팅
    초기 셋팅

---
+++ src/main/java/com/example/FastcampusSpringBasics/FastcampusSpringBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.FastcampusSpringBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusSpringBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcampusSpringBasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FastcampusSpringBasics/FastcampusSpringBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FastcampusSpringBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusSpringBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
