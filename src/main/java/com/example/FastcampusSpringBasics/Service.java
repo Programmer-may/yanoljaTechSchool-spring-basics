7408bd491cc9d7a4d077eea2b72b5a29b15b1ab0
JuHyeonPark <parkjh080@naver.com>
2023-10-12 22:35:15 +0900

    feat(study): Service.java 생성 2023-10-12
    자바 프로젝트에선 DB 접근을 DAO 클래스를 만들어 로직을 처리하였다. 하지만 스프링에선 Repository에서 이걸 실행한다.
    명칭에 주의 해야한다. 싱글톤 패턴을 지키기 위해 스프링 컨테이너에 Repository를 빈으로 등록하고 의존성 주입을 통하여
    객체를 하나만 만들어서 일을 할 수 있게 해야한다.

---
+++ src/main/java/com/example/FastcampusSpringBasics/Service.java
@@ -0,0 +1,10 @@
+package com.example.FastcampusSpringBasics;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Service {
+
+    @Autowired
+    Repository repository;
+
+}
