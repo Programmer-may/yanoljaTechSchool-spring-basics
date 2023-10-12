39fad061f9c965928c329e1c8463ac8cd3eafc52
JuHyeonPark <parkjh080@naver.com>
2023-10-12 22:39:14 +0900

    feat(study): DBRepositoty.java 생성 2023-10-12
    가상의 DB Repository 였던 MapRepository를 대신하여 실제 DB Repository

---
+++ src/main/java/com/example/FastcampusSpringBasics/DBRepository.java
@@ -0,0 +1,14 @@
+package com.example.FastcampusSpringBasics;
+
+public class DBRepository implements Repository{
+
+    @Override
+    public void save(String data) {
+
+    }
+
+    @Override
+    public void findById(int id) {
+
+    }
+}
