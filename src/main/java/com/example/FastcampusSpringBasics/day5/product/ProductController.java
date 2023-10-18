fb0e45fe891ab4b61fc5fae0102c85bf7a917ad2
JuHyeonPark <parkjh080@naver.com>
2023-10-18 21:27:15 +0900

    feat(study): DDD공부 - 컨트롤러 2023-10-18
    쇼핑몰을 예시로 했을때 도메인 단위로 컨트롤러 생성

---
+++ src/main/java/com/example/FastcampusSpringBasics/day5/product/ProductController.java
@@ -0,0 +1,14 @@
+package com.example.FastcampusSpringBasics.day5.product;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+public class ProductController {
+
+    /**
+     * 클라이언트에게 데이터를 전달받는 방법 3가지
+     * 쿼리 스트링 : URL 값을 들고 들어오겠다 ex. 특정 상품 상세 조회 : Get /products?id= , /products/{id}
+     * http 메세지 바디 : #JSON <-> #직렬화/역직렬화 <-> 자바 객체
+     */
+}
