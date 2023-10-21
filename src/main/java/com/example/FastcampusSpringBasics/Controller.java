66abd15d06c90eba3df25af411ff50c379d14727
JuHyeonPark <parkjh080@naver.com>
2023-10-21 20:04:24 +0900

    feat(study): MVC 패턴 공부 2023-10-18
    컨트롤러 계층 공부

---
+++ src/main/java/com/example/FastcampusSpringBasics/Controller.java
@@ -0,0 +1,12 @@
+package com.example.FastcampusSpringBasics;
+
+// URL 요청을 처음 받는 친구
+public class Controller {
+
+    /**
+     * 클래스 구성요소 3개
+     * 1) 필드
+     * 2) 생성자
+     * 3) 메서드 // URL 요청 = 메서드 호출
+     */
+}
