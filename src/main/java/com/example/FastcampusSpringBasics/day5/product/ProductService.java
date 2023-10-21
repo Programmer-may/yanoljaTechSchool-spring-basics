a183a4bb12b4d2aa4a04c15f20c9f17e3fb8b68b
JuHyeonPark <parkjh080@naver.com>
2023-10-21 20:04:05 +0900

    feat(study): MVC 패턴 공부 2023-10-18
    컨트롤 계층에서 서비스 계층 생성자 주입

--- src/main/java/com/example/FastcampusSpringBasics/day5/product/ProductService.java
+++ src/main/java/com/example/FastcampusSpringBasics/day5/product/ProductService.java
@@ -1,4 +1,15 @@
 package com.example.FastcampusSpringBasics.day5.product;
 
+import java.util.List;
+
 public class ProductService {
+    public void saveProduct() {
+    }
+
+    public void saveAllProducts() {
+    }
+
+    public List<ProductDto> findAllProducts() {
+        return null;
+    }
 }
