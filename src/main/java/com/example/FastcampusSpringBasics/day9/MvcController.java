9fceb2992b4871a3e5a2104a1ba688699df09002
JuHyeonPark <parkjh080@naver.com>
2023-11-08 22:23:22 +0900

    feat: view 페이지로 가게하는 controller
    view 페이지로 가게하는 controller

---
+++ src/main/java/com/example/FastcampusSpringBasics/day9/MvcController.java
@@ -0,0 +1,18 @@
+package com.example.FastcampusSpringBasics.day9;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class MvcController {
+//
+//    @GetMapping("/home")
+//    public String home() {
+//        return "home";
+//    }
+//
+//    @GetMapping("/hello")
+//    public String hello() {
+//        return "hello";
+//    }
+//}
