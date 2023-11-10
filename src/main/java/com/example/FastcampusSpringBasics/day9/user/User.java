648f115050c23c3e137c4304de4f3f57110772e3
JuHyeonPark <parkjh080@naver.com>
2023-11-10 23:44:44 +0900

    feat: User 클래스 작성
    Spring security 실습을 위한 User 객체가 될 클래스 작성

---
+++ src/main/java/com/example/FastcampusSpringBasics/day9/user/User.java
@@ -0,0 +1,79 @@
+package com.example.FastcampusSpringBasics.day9.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class User implements UserDetails {
+
+    private int id; // PK
+
+    private String email; // 아이디
+    private String password;
+    private String role;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // getEmail
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    // getRole
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // User가 가지고 있는 권한(Role)들
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role));
+
+        return authorities;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
