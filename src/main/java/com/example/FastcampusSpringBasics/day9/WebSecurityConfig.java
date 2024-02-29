b8a3902b607647ec0e1c074f4349b253bebb2d26
JuHyeonPark <parkjh080@naver.com>
2024-02-29 20:10:00 +0900

    refactor: 시큐리티 마이그레이션으로 인한 수정
    시큐리티 마이그레이션으로 인한 수정

--- src/main/java/com/example/FastcampusSpringBasics/day9/WebSecurityConfig.java
+++ src/main/java/com/example/FastcampusSpringBasics/day9/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.FastcampusSpringBasics.day9;
 
+import com.example.FastcampusSpringBasics.day9.security.JwtAuthenticationFilter;
+import com.example.FastcampusSpringBasics.day9.security.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,37 +9,51 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-        throws Exception {
-        http.authorizeHttpRequests()
-            .requestMatchers("/", "/home", "/join")
-            .permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .formLogin()
-            .loginPage("/login")
-            .permitAll();
-
-        return http.build();
+    public JwtTokenProvider jwtTokenProvider() {
+        return new JwtTokenProvider();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-            User.withDefaultPasswordEncoder()
-                .username("songa")
-                .password("123")
-                .roles("ADMIN")
-                .build();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+        throws Exception {
+        http.authorizeHttpRequests()
+            .requestMatchers("/", "/home", "/join", "/login")
+            .permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().disable()
+            .csrf().disable()
+            .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider()),
+                UsernamePasswordAuthenticationFilter.class);
 
-        return new InMemoryUserDetailsManager(user);
+        return http.getOrBuild();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("songa")
+//                        .password("123")
+//                        .roles("ADMIN")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 }
